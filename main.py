60bdfcbb34f8dba2c8b26ff1164bf9a8c38d4b9f
darren7753 <matthewdarren7753@gmail.com>
2023-05-17 23:28:11 +0700

    fixed the selenium error

--- main.py
+++ main.py
@@ -3,13 +3,34 @@ import pandas as pd
 import time
 from datetime import datetime
 from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.core.utils import ChromeType
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
 
 # Loading the data containing information about my Streamlit apps
 df = pd.read_excel("streamlit_apps.xlsx")
 df = df.sort_values("Name").reset_index(drop=True)
 
+# Setting up the browser
+chrome_service = Service(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install())
+
+chrome_options = Options()
+options = [
+    "--headless",
+    "--disable-gpu",
+    "--window-size=1920,1200",
+    "--ignore-certificate-errors",
+    "--disable-extensions",
+    "--no-sandbox",
+    "--disable-dev-shm-usage"
+]
+for option in options:
+    chrome_options.add_argument(option)
+
+driver = webdriver.Chrome(service=chrome_service, options=chrome_options)
+
 # Visiting each of my Streamlit app and noting the timestamp when visiting
-driver = webdriver.Chrome()
 timestamps = []
 
 for i,j in zip(df["Name"], df["Link"]):
@@ -30,7 +51,7 @@ markdown_df = markdown_df.drop("Link", axis=1)
 # Updating the README file
 intro_text = '''# Streamlit Stay Up
 
-This is a simple Python script automated by GitHub Actions to visit my Streamlit apps once every Monday and Friday to prevent them from going to sleep.
+This is a simple Python script automated by GitHub Actions to visit my Streamlit apps every Monday and Friday to prevent them from going to sleep.
 
 '''
 with open("README.md", "w", encoding="utf-8") as f:
